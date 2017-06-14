a1b0c5bbd4ade4620bb403b1c08b5d8ceb7e99cf
Guo Xiang Tan <tgx_world@hotmail.com>
2017-06-14 09:32:43 +0900

    Don't mix tabs with spaces for indentation.

--- app/controllers/discourse_voting/votes_controller.rb
+++ app/controllers/discourse_voting/votes_controller.rb
@@ -1,5 +1,5 @@
 module DiscourseVoting
-	class VotesController < ::ApplicationController
+  class VotesController < ::ApplicationController
     before_filter :ensure_logged_in
 
     def who
@@ -10,8 +10,8 @@ module DiscourseVoting
       render json: MultiJson.dump(who_voted(topic))
     end
 
-		def add
-			topic = Topic.find_by(id: params["topic_id"])
+    def add
+      topic = Topic.find_by(id: params["topic_id"])
 
       raise Discourse::InvalidAccess if !topic.can_vote?
       guardian.ensure_can_see!(topic)
@@ -28,38 +28,38 @@ module DiscourseVoting
         voted = true
       end
 
-			obj = {
+      obj = {
         can_vote: !current_user.reached_voting_limit?,
-				vote_limit: current_user.vote_limit,
-				vote_count: topic.custom_fields["vote_count"].to_i,
-				who_voted: who_voted(topic),
+        vote_limit: current_user.vote_limit,
+        vote_count: topic.custom_fields["vote_count"].to_i,
+        who_voted: who_voted(topic),
         alert:  current_user.alert_low_votes?,
         votes_left: [(current_user.vote_limit - current_user.vote_count), 0].max
-			}
+      }
 
-			render json: obj, status: voted ? 200 : 403
-		end
+      render json: obj, status: voted ? 200 : 403
+    end
 
-		def remove
-			topic = Topic.find_by(id: params["topic_id"])
+    def remove
+      topic = Topic.find_by(id: params["topic_id"])
 
       guardian.ensure_can_see!(topic)
 
-			current_user.custom_fields["votes"] = current_user.votes.dup - [params["topic_id"].to_s]
-			current_user.save
+      current_user.custom_fields["votes"] = current_user.votes.dup - [params["topic_id"].to_s]
+      current_user.save
 
       update_vote_count(topic)
 
-			obj = {
+      obj = {
         can_vote: !current_user.reached_voting_limit?,
-				vote_limit: current_user.vote_limit,
-				vote_count: topic.custom_fields["vote_count"].to_i,
-				who_voted: who_voted(topic),
+        vote_limit: current_user.vote_limit,
+        vote_count: topic.custom_fields["vote_count"].to_i,
+        who_voted: who_voted(topic),
         votes_left: [(current_user.vote_limit - current_user.vote_count), 0].max
-			}
+      }
 
-			render json: obj
-		end
+      render json: obj
+    end
 
     protected
 
@@ -68,7 +68,7 @@ module DiscourseVoting
       topic.save
     end
 
-		def who_voted(topic)
+    def who_voted(topic)
       return nil unless SiteSetting.voting_show_who_voted
 
       users = User.where("id in (
@@ -76,7 +76,7 @@ module DiscourseVoting
       )", params[:topic_id].to_i.to_s)
 
       ActiveModel::ArraySerializer.new(users, scope: guardian, each_serializer: BasicUserSerializer)
-		end
+    end
 
-	end
+  end
 end
