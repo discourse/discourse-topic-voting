b92385aadab510104d21ed4eb43ee804b7a85b56
Jarek Radosz <jradosz@gmail.com>
2020-10-06 18:04:26 +0200

    FIX: Delete the votes on user removal (#59)
    There are some assumptions in the code that `vote.user` exists. It makes sense to boop the votes out of existence when their owner is destroyed.

--- lib/discourse_voting/user_extension.rb
+++ lib/discourse_voting/user_extension.rb
@@ -3,7 +3,7 @@
 module DiscourseVoting
   module UserExtension
     def self.prepended(base)
-      base.has_many :votes, class_name: 'DiscourseVoting::Vote'
+      base.has_many :votes, class_name: 'DiscourseVoting::Vote', dependent: :destroy
     end
   end
 end
