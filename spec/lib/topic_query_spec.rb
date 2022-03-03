25dec247d3fef6b4eeb439145e7b6e9bc1201603
Vinoth Kannan <svkn.87@gmail.com>
2022-03-04 01:45:13 +0530

    FIX: add second order by column to preserve order while paging.
    If the number of votes are same and the second order-by column not specified then postgres makes no guarantee about what the ordering will be.

--- spec/lib/topic_query_spec.rb
+++ spec/lib/topic_query_spec.rb
@@ -23,4 +23,16 @@ describe TopicQuery do
   it "returns topics voted by user" do
     expect(TopicQuery.new(user0, { state: 'my_votes' }).list_latest.topics.map(&:id)).to eq([topic1.id])
   end
+
+  it "orders topic by bumped_at if votes are equal" do
+    topic2 = Fabricate(:topic, category: category1, bumped_at: 2.hours.ago)
+    DiscourseVoting::TopicVoteCount.create!(topic_id: topic2.id, votes_count: 2)
+    topic3 = Fabricate(:topic, category: category1, bumped_at: 3.hours.ago)
+    topic4 = Fabricate(:topic, category: category1, bumped_at: 1.hour.ago)
+
+    opts = { order: 'votes', per_page: 2, category: category1.slug }
+    expect(TopicQuery.new(user0, opts).list_latest.topics.map(&:id)).to eq([topic2.id, topic1.id])
+    expect(TopicQuery.new(user0, opts.merge(page: 1)).list_latest.topics.map(&:id)).to eq([topic0.id, topic4.id])
+    expect(TopicQuery.new(user0, opts.merge(page: 2)).list_latest.topics.map(&:id)).to eq([topic3.id])
+  end
 end
