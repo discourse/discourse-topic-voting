b4d3f3bdce203d103c76dc8201a8f067895399fc
Ted Johansson <ted@discourse.org>
2023-03-07 12:52:14 +0800

    DEV: Add inverse declaration for  association

--- spec/models/discourse_voting/category_setting_spec.rb
+++ spec/models/discourse_voting/category_setting_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 describe DiscourseTopicVoting::CategorySetting do
   fab!(:category) { Fabricate(:category) }
 
+  it { is_expected.to belong_to(:category).inverse_of(:discourse_topic_voting_category_setting) }
+
   describe "logs category setting changes" do
     it "logs changes when voting is enabled/disabled" do
       DiscourseTopicVoting::CategorySetting.create!(category: category)
