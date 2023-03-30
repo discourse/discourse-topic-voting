83877c5921a1850e99e94ac2415107b3c844c07d
Natalie Tay <natalie.tay@discourse.org>
2023-03-30 12:53:44 +0800

    FIX: Don't remove custom fields when adding a new one to category serializer (#150)

---
+++ spec/serializers/category_serializer_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe CategorySerializer do
+  fab!(:category) { Fabricate(:category) }
+
+  it "does not return enable_topic_voting voting disabled" do
+    SiteSetting.voting_enabled = false
+
+    json = CategorySerializer.new(category, root: false).as_json
+
+    expect(json[:custom_fields]).to eq({})
+  end
+
+  it "returns enable_topic_voting when voting enabled" do
+    SiteSetting.voting_enabled = true
+
+    json = CategorySerializer.new(category, root: false).as_json
+
+    expect(json[:custom_fields]).to eq({ "enable_topic_voting" => false })
+  end
+end
