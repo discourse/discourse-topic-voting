9b9b4e1e0c159d6654a6b9c397caeab8cb035911
Nat <natalie.tay@discourse.org>
2023-03-30 12:32:50 +0800

    returns

--- spec/serializers/category_serializer_spec.rb
+++ spec/serializers/category_serializer_spec.rb
@@ -13,7 +13,7 @@ describe CategorySerializer do
     expect(json[:custom_fields]).to eq({})
   end
 
-  it "return enable_topic_voting when voting enabled" do
+  it "returns enable_topic_voting when voting enabled" do
     SiteSetting.voting_enabled = true
 
     json = CategorySerializer.new(category, root: false).as_json
