b41cee2941be91d88487226158d799a7488dd24e
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-09-26 11:35:09 +1000

    FIX: update after changes

--- spec/fabricators/topic_voting_hook_fabricator.rb
+++ spec/fabricators/topic_voting_hook_fabricator.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Fabricator(:topic_voting_web_hook, from: :web_hook) do
-  transient topic_voting_hook: WebHookEventType.find_by(name: "topic_upvote")
-
-  after_build do |web_hook, transients|
-    web_hook.web_hook_event_types = [transients[:topic_voting_hook]]
+  after_build do |web_hook|
+    web_hook.web_hook_event_types = WebHookEventType.where(name: %w[topic_upvote topic_unvote])
   end
 end
